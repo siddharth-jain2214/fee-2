b282e0a78b591673fe0ecc0c4431c216b8a8d2f6
siddharth-jain2214 <siddharthj634@gmail.com>
2024-08-30 12:49:30 +0530

    string and array methods

---
+++ class/javascript/array.js
@@ -0,0 +1,7 @@
+let arr=[1,2,3,4,5]
+arr[2]=5;
+console.log(arr)
+console.log(arr.length)
+
+let arr1=[[1,2],[2,3],[3,4]]
+console.log(arr[0][1])
---
+++ class/javascript/string.js
@@ -0,0 +1,16 @@
+let str='Siddharth Jain'
+str[3]='t'
+console.log(str)
+
+
+let new1="       Siddharth"
+console.log(new1.trim)
+
+let msg='fool'
+console.log(msg.repeat(100))
+
+let fname="siddharth"
+let lname="jain"
+
+console.log("Mr."+" "+fname+" "+lname)
+console.log("Mr. ${fname} ${lname}")
